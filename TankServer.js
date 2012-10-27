2d6ad5bb7e8fbef9e7098a3021cb727bf6486ed4
Vandhanaa <vandhanaa91@gmail.com>
2012-10-27 17:21:34 +0800

    http server - fixed
    Call localhost:8080 from the browser

--- TankServer.js
+++ TankServer.js
@@ -79,14 +79,52 @@ function TankServer() {
 			// Initialization
 			port = Game.PORT;
 			var http = require('http');
-
+			var fs = require('fs');
+			var path = require('path');
 			// change log level to 3 for debugging messages
 			
 			//.listen(port, { 'log level':3  );
 
-			var server = http.createServer().listen(port, function() {
-				console.log('Listening at: http://localhost:8080');
+			var server = http.createServer(function(request, response) {
+				console.log("starting request");
+				console.log(request);
+				var filePath = '.' + request.url;
+				if(filePath == './')
+					filePath = './tank_fight.html';
+
+				var extname = path.extname(filePath);
+				var contentType = 'text/html';
+				switch (extname) {
+					case '.js':
+						contentType = 'text/javascript';
+						break;
+					case '.css':
+						contentType = 'text/css';
+						break;
+				}
+				path.exists(filePath, function(exists){
+					if (exists) {
+						fs.readFile(filePath, function(error, content) {
+							if(error) {
+								response.writeHead(500);
+								response.end();
+							}
+							else {
+								response.writeHead(200, {"Content-Type": contentType});
+		 						response.end(content, 'utf-8');
+							}
+						});
+					}
+					else {
+						response.writeHead(404);
+						response.end();
+					}
+				});				
 			});
+
+			server.listen(port);
+			console.log('Listening at: http://localhost:8080');
+
 			io = require('socket.io').listen(server, {'log level':3});
 			// function(req, res) { 
 			// 	res.writeHead(200, { 'Content-type': 'text/html'});
